42d5a7ee7e01bd6c16844bdbb614d00c1ac43575
Harsh Dwivedi <harsh007dwivedi@gmail.com>
2021-03-28 03:50:40 +0530

    first commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,135 @@
+import React from "react";
+import BgImage from "./images/img-bg.svg";
+import MyImage from "./images/Exxclusion-3.png";
+import YCircle from "./images/yellow-circle.svg";
+import OCircle from "./images/orange-circle.svg";
+import BPattern from "./images/blue-pattern.svg";
+import Programme from "./images/programming.svg";
+import Design from "./images/graphic-design.svg";
+// ICONS
+import AI from "./images/icons/ai.svg";
+import PS from "./images/icons/ps.svg";
+import XD from "./images/icons/xd.svg";
+import ReactIcon from "./images/icons/react.svg";
+import HTML from "./images/icons/html.svg";
+import CSS from "./images/icons/css.svg";
+import JS from "./images/icons/js.svg";
+
+// Contact Icons
+import Email from "./images/icons/email.svg";
+import Call from "./images/icons/phone-call.svg";
+
+import "../index.css";
+
+function HeroSection(props) {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="name-div">
+          <h1 className="name animate__animated animate__bounceInLeft animate__slow">
+            Harsh Dwivedi
+          </h1>
+        </div>
+        <div className="row">
+          <div className="col-lg-3 col-md-4 p-1 left-div">
+            <div className="left-content-div">
+              <div>
+                <img src={YCircle} className="y-circle" alt="" />
+              </div>
+              <div className="programme-div">
+                <div className="programme-img-div">
+                  <img src={Programme} className="programme" alt="" />
+                </div>
+                <h5 className="programme-heading">A Developer</h5>
+                <a
+                  href="https://github.com/Zodiac007"
+                  className="btn btn-secondary p-btn"
+                  target="_blank"
+                >
+                  Development Work
+                </a>
+              </div>
+              <div className="email-div">
+                <img src={Email} className="email" alt="" />
+                <a href="#">
+                  <p>harsh007dwivedi@gmail.com</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-4 p-2 center-div">
+            <div className="center-img-div">
+              <img src={BgImage} className="bg-image" alt="" />
+              <img src={MyImage} className="my-image" alt="" />
+              {/* Icons */}
+              <img
+                src={HTML}
+                className="html"
+                alt=""
+              />
+              <img
+                src={CSS}
+                className="css"
+                alt=""
+              />
+              <img
+                src={JS}
+                className="js"
+                alt=""
+              />
+              <img
+                src={ReactIcon}
+                className="react"
+                alt=""
+              />
+              <img
+                src={AI}
+                className="ai"
+                alt=""
+              />
+              <img
+                src={PS}
+                className="ps"
+                alt=""
+              />
+              <img
+                src={XD}
+                className="xd"
+                alt=""
+              />
+            </div>
+
+            <div className="hidden-div">
+              <img src={Email} className="email" alt="" />
+              <a href="#">
+                <p>harsh007dwivedi@gmail.com</p>
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-4 p-3 right-div">
+            <div className="right-content-div">
+              <div>
+                <img src={OCircle} className="o-circle" alt="" />
+              </div>
+              <div className="design-div">
+                <div className="design-img-div">
+                  <img src={Design} className="design" alt="" />
+                </div>
+                <h5 className="design-heading">A Designer</h5>
+                <a
+                  href="https://drive.google.com/drive/folders/1P9uYbyEnH1RjXLh62_5Qi4aFOEG5qzHK?usp=sharing"
+                  className="btn btn-secondary d-btn"
+                  target="_blank"
+                >
+                  Designing Work
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/Nav.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Logo from "./images/logo.svg";
+import Twitter from "./images/icons/twitter.svg";
+import Facebook from "./images/icons/facebook.svg";
+import Linkedin from "./images/icons/linkedin.svg";
+import Instagram from "./images/icons/instagram.svg";
+import "../index.css";
+
+function Nav() {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row nav-row">
+          <div className="col-md-6">
+            <a href="#">
+              <img
+                src={Logo}
+                className="animate__animated logo animate__bounceInLeft animate__delay-2s"
+                height="70"
+                width="70"
+                alt="list item 1"
+              />
+            </a>
+          </div>
+          <div className="col-md-6">
+            <h6>Social Media</h6>
+
+            <ul
+              id="horizontal-list"
+              className="animate__animated animate__bounceInRight animate__delay-2s"
+            >
+              <li>
+                <a
+                  href="https://www.facebook.com/harsh.dwivedi.92123"
+                  target="_blank"
+                >
+                  <img
+                    src={Facebook}
+                    className="social-icon"
+                    height="20"
+                    width="20"
+                    alt="list item 1"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/harsh007dwivedi" target="_blank">
+                  <img
+                    src={Twitter}
+                    className="social-icon"
+                    height="20"
+                    width="20"
+                    alt="list item 2"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/harsh-dwivedi-949035166/"
+                  target="_blank"
+                >
+                  <img
+                    src={Linkedin}
+                    className="social-icon"
+                    height="20"
+                    width="20"
+                    alt="list item 3"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/harsh777dwivedi/"
+                  target="_blank"
+                >
+                  <img
+                    src={Instagram}
+                    className="social-icon"
+                    height="20"
+                    width="20"
+                    alt="list item 4"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
